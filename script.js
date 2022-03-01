d018ed676531cdccb48a69d987660f8689805e8c
Ayaz Aybek <87331452+frostaybek@users.noreply.github.com>
2022-03-01 15:54:40 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const div = document.getElementById('offline')
-
-window.addEventListener('online', () => {
-    div.style.display = 'none'
-})
-
-window.addEventListener('offline', () => {
-    div.style.display = 'block'
-})
